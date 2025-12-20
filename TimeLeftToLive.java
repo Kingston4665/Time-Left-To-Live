75c5f45c43b8adf1e568c532a3065ed5f27a2c90
Kingston <kingstondashfree@gmail.com>
2025-12-19 19:30:05 -0500

    Edits: -Added comments for better readability -Split longer lines into 2 lines and added spacing for improved readability -Added the warning supression so it would stop making the same suggestion over and over -The project auto generated the .class file and it reappears everytime I run my project so thats there now -Added the Explanation text file to explain how the random number generation works

--- TimeLeftToLive.java
+++ TimeLeftToLive.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
     
 public class TimeLeftToLive {
+@SuppressWarnings("ConvertToTryWithResources")
 public static void main(String args[]) {
    
     Scanner Scanner = new Scanner (System.in);
@@ -12,12 +13,17 @@ public static void main(String args[]) {
     int age = Scanner.nextInt();
     
     while (age <= 0 || age >= 105) {
-
+    
+    //Only runs when the inputed age is less than or equal to 0 or greater than or equal to 105
     System.out.println("\nEnter your ACTUAL age(then press enter): ");
     age = Scanner.nextInt();
     
         }
     
+    //Gives a random floating-point number between 0 (inclusive) and the number it's being mulitplied by (exclusive), 
+    //for example with "Months": which would result in a number between 0 and 11, 
+    //then uses integer casting to switch it from a double to an int, allowing only whole numbers 
+    //**SEE "Explanation" TEXT FILE TO LEARN MORE!!**
     int Years = (int)(Math.random() * (105-age));
     int Months = (int)(Math.random() * 12);
     int Weeks = (int)(Math.random() * 4);
@@ -25,9 +31,11 @@ public static void main(String args[]) {
     int Minutes = (int)(Math.random() * 60);
     int Seconds = (int)(Math.random() * 60);
    
-    System.out.println("\n" + name + ", you have " + Years + " Year(s), " + Months + " Month(s), " + Weeks + " Week(s), " + Days + " Day(s), " + Minutes + " Minute(s) and " + Seconds + " Second(s)" + " left to live!");
+    System.out.println("\n" + name + ", you have " + Years + " Year(s), " + Months + " Month(s), " + 
+        Weeks + " Week(s), " + Days + " Day(s), " + Minutes + " Minute(s) and " + Seconds + " Second(s)" + " left to live!");
 
     Scanner.close();    
       
   }
+
 }
